6a9fdb5f70331a18e217c76e18ea5bfb8e307b24
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-28 23:33:38 +0200

    Project Setup

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyDeFVM2HQYjcy-MEvbgs6faNmE4tCavsek',
+	authDomain: 'fir-javascript-crud-84695.firebaseapp.com',
+	projectId: 'fir-javascript-crud-84695',
+	storageBucket: 'fir-javascript-crud-84695.appspot.com',
+	messagingSenderId: '450422570177',
+	appId: '1:450422570177:web:a9a3628db63c91c75de840',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
